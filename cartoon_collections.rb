bab011ba332e871f429deb0260a8e9f71376ef3d
Ariel J <arieljakubowski@gmail.com>
2019-05-21 02:03:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ def long_planeteer_calls(short_words)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese
   cheese_types = ["cheddar", "gouda", "camembert"]
+
 end
